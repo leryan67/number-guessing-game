d92f9e652a51934fe8d30d36ff1ba9b9d9661533
Ryan Le <leryan027@gmail.com>
2026-06-07 16:22:58 -0700

    Added conditions for guesses

--- GuessingGame.java
+++ GuessingGame.java
@@ -12,6 +12,23 @@ public class GuessingGame {
         while (isGuessing) {
             System.out.print("Guess a number between 1-100: ");
             int num = userGuess.nextInt();
+
+            if (num < 0 || num > 100) {
+                guessCounter++;
+                System.out.println("This guess is out of the range. Guess again.");
+            }
+            else if (num < computerGuess) {
+                guessCounter++;
+                System.out.println("Guess higher.");
+            }
+            else if (num > computerGuess) {
+                guessCounter++;
+                System.out.println("Guess lower.");
+            }
+            else {
+                guessCounter++;
+                System.out.println("Congrats! You guessed the number in " + guessCounter + " attempts!");
+            }
         }
     }
     
